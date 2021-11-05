03fd84a23f87950ff018f01a6bfe4da977c9caa5
github_titaniamela <shaniaslsbl@gmail.com>
2021-11-05 22:22:13 +0700

    almost done

--- client/src/components/Form_profil_user.vue
+++ client/src/components/Form_profil_user.vue
@@ -8,26 +8,31 @@
             <div class="col-md flex-row d-md-block flex-wrap">
                 <!-- <div class="form-data" v-if="!submitted" id="form1"> -->
                 <!-- <div class="form-data" @submit.prevent="loginUser" id="form1"> -->
-                <form @submit.prevent="loginUser">
-                    <div class="forms-inputs mb-4"> 
+                <form @submit.prevent="registerUser">
+                    <div class="forms-inputs mb-4">  
                         <span>Nama</span> 
-                        <input id="email_user" autocomplete="off" type="email" v-model="login.email" v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(login.email) && emailBlured}" v-on:blur="emailBlured = true" placeholder="Ketik email di sini">
-                        <div class="invalid-feedback">Email harus valid!</div>
+                        <input id="name_user" autocomplete="off" type="text" v-model="register.name" v-bind:class="{'form-control':true, 'is-invalid' : !validUname(uname) && unameBlured}" v-on:blur="unameBlured = true" placeholder="Shalvina">
                     </div>
                     <div class="forms-inputs mb-4"> 
                         <span>Email</span> 
-                        <input id="email_user" autocomplete="off" type="email" v-model="login.email" v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(login.email) && emailBlured}" v-on:blur="emailBlured = true" placeholder="Ketik email di sini">
+                        <input id="email_user" autocomplete="off" type="text" v-model="register.email" v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlured}" v-on:blur="emailBlured = true" placeholder="shalvina@gmail.com">
                         <div class="invalid-feedback">Email harus valid!</div>
                     </div>
                     <div class="forms-inputs mb-4"> 
-                        <span>Kata Sandi</span>
-                        <input id="pass_user" autocomplete="off" type="password" v-model="login.password" v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(login.password) && passwordBlured}" v-on:blur="passwordBlured = true" placeholder="Ketik kata sandi di sini">
+                        <span>Kata Sandi Sekarang</span>
+                        <input id="pass_user" autocomplete="off" type="password" v-model="register.password" v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlured}" v-on:blur="passwordBlured = true" placeholder="Ketik kata sandi sekarang">
+                        <div class="invalid-feedback">Password harus benar!</div>
+                    </div>
+                    <div class="forms-inputs mb-4"> 
+                        <span>Kata Sandi Baru</span>
+                        <input id="pass_user" autocomplete="off" type="password" v-model="register.password" v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlured}" v-on:blur="passwordBlured = true" placeholder="Ketik kata sandi baru">
                         <div class="invalid-feedback">Password minimal 8 karakter!</div>
                     </div>
-                    <div class="mb-3"> 
-                        <!-- <button v-on:click.stop.prevent="submit" type="submit" class="btn bg-light-orange w-100 br-10">Masuk</button>  -->
-                        <button type="submit" class="btn bg-light-orange w-100 br-10">Simpan</button> 
-                    </div>
+                    <div class="mb-3 d-flex justify-content-end"> 
+                        <!-- <button v-on:click.stop.prevent="submit" type="submit" class="btn bg-light-orange w-100 br-10">Daftar</button>  -->
+                        <button type="button" class="btn bg-light-orange me-3 br-10" style="background-color:#D65A40">Hapus Akun</button> 
+                        <button type="submit" class="btn bg-light-orange br-10">Simpan</button>
+                    </div> 
                 </form>
                 <!-- </div> -->
             </div>
@@ -37,29 +42,40 @@
 
 <script>
 import axios from 'axios'
-const LOGIN_API_URL = `http://localhost:3000/auth/users/login`
+const REGISTER_API_URL = `http://localhost:3000/auth/users/register`
 
 export default {
     name:'Form_login',
     data: function () {
         return {
+            uname :"",
+            unameBlured : false,
+            email : "",
             emailBlured : false,
             valid : false,
             submitted : false,
+            password:"",
             passwordBlured:false,
-            login: {
+            register: {
+                username: "",
                 email: "",
                 password: ""
-            }
-        }   
+            },
+        }
     },
     methods:{
         validate : function(){
+            this.unameBlured = true;
             this.emailBlured = true;
             this.passwordBlured = true;
-            if( this.validEmail(this.email) && this.validPassword(this.password)){
+            if( this.validUname(this.uname) && this.validEmail(this.email) && this.validPassword(this.password)){
                 this.valid = true;
                 }
+        },
+        validUname : function(uname) {
+            if(uname.length > 0){
+                return true;
+            }
         },
         validEmail : function(email) {
             var re = /(.+)@(.+){2,}\.(.+){2,}/;
@@ -78,6 +94,23 @@ export default {
                 this.submitted = true;
             }
         },
+        registerUser(e) {
+      e.preventDefault()
+      if (this.register.password.length > 0){
+        axios.post(REGISTER_API_URL, this.register)
+        .then(response => {
+          localStorage.setItem('token',response.data.token)
+
+          if (localStorage.getItem('token') != null){
+            this.$emit('loggedIn')
+            this.$router.push('/')    
+          }
+        })
+        .catch(error => {
+            console.error(error)
+        })
+      } 
+    },
     }
 }
 </script>
