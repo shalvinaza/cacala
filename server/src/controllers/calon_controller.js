933a48d0cb5723812a0e83da0768a913dbb0ed47
aithrajbouty <aithrabouty@gmail.com>
2021-11-10 12:14:53 +0700

    add get calon by user and get calon by admin token

--- server/src/controllers/calon_controller.js
+++ server/src/controllers/calon_controller.js
@@ -3,7 +3,7 @@ const { pool } = require("../dbConfig")
 exports.selectAllCalon = async (req, res) => {
    try{
       const calon = await pool.query(
-         "select calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.username, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi;"
+         "select c.id_calon, c.nama, c.foto, c.slogan, a.id_admin, j.jabatan_tujuan, k.kota, p.provinsi, pend.nama_institusi, pend.detail_pendidikan, pend.tahun_mulai_pendidikan, pend.tahun_selesai_pendidikan, pek.nama_pekerjaan, pek.detail_pekerjaan, pek.tahun_mulai_pekerjaan, pek.tahun_selesai_pekerjaan FROM calon c JOIN admins a on c.id_admin = a.id_admin JOIN jabatan j on c.id_jabatan = j.id_jabatan JOIN kota k on c.id_dapil_kota = k.id_kota JOIN provinsi p on k.id_provinsi = p.id_provinsi JOIN riwayat_pendidikan pend on pend.id_calon = c.id_calon JOIN riwayat_pekerjaan pek on pek.id_calon = c.id_calon;"
       )
 
       res.json(calon.rows)
@@ -31,11 +31,11 @@ exports.addCalon = async (req, res) => {
    }
 }
 
-exports.selectSpecificCalon = async (req, res) => {
+exports.selectCalonByUser = async (req, res) => {
    const {id} = req.params
    try{
       const calon = await pool.query(
-         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.username, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi WHERE id_calon = $1;", [
+         "select c.id_calon, c.nama, c.foto, c.slogan, a.id_admin, j.jabatan_tujuan, k.kota, p.provinsi, pend.nama_institusi, pend.detail_pendidikan, pend.tahun_mulai_pendidikan, pend.tahun_selesai_pendidikan, pek.nama_pekerjaan, pek.detail_pekerjaan, pek.tahun_mulai_pekerjaan, pek.tahun_selesai_pekerjaan FROM calon c JOIN admins a on c.id_admin = a.id_admin JOIN jabatan j on c.id_jabatan = j.id_jabatan JOIN kota k on c.id_dapil_kota = k.id_kota JOIN provinsi p on k.id_provinsi = p.id_provinsi JOIN riwayat_pendidikan pend on pend.id_calon = c.id_calon JOIN riwayat_pekerjaan pek on pek.id_calon = c.id_calon WHERE c.id_calon = $1;", [
          id
       ])
 
@@ -49,7 +49,7 @@ exports.selectCalonByKota = async (req, res) => {
    const { id_kota } = req.params
    try{
       const calon = await pool.query(
-         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.username, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi WHERE id_kota = $1;",[
+         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.id_admin, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi WHERE kota.id_kota = $1;",[
             id_kota
          ])
       
@@ -63,7 +63,7 @@ exports.selectCalonByJabatan = async (req, res) => {
    const { id_jabatan } = req.params
    try{
       const calon = await pool.query(
-         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.username, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi WHERE calon.id_jabatan = $1;",[
+         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.id_admin, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi WHERE calon.id_jabatan = $1;",[
             id_jabatan
          ])
       
@@ -77,7 +77,7 @@ exports.selectCalonByPartai = async (req, res) => {
    const { id_partai } = req.params
    try{
       const calon = await pool.query(
-         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.username, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi, partai.nama_partai FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi JOIN partai_calon ON partai_calon.id_calon = calon.id_calon JOIN partai ON partai_calon.id_partai = partai.id_partai WHERE partai_calon.id_partai = $1;",[
+         "SELECT calon.id_calon, calon.nama, calon.foto, calon.slogan, admins.id_admin, jabatan.jabatan_tujuan, kota.kota, provinsi.provinsi, partai.nama_partai FROM calon JOIN admins on calon.id_admin = admins.id_admin JOIN jabatan on calon.id_jabatan = jabatan.id_jabatan JOIN kota on calon.id_dapil_kota = kota.id_kota JOIN provinsi on kota.id_provinsi = provinsi.id_provinsi JOIN partai_calon ON partai_calon.id_calon = calon.id_calon JOIN partai ON partai_calon.id_partai = partai.id_partai WHERE partai_calon.id_partai = $1;",[
             id_partai
          ])
       
@@ -94,6 +94,84 @@ exports.selectPartai = async (req, res) => {
       )
 
       res.json(calon.rows)
+   } catch(err) {
+      res.json({ message: err })
+   }
+}
+
+// exports.selectPartaiCalon = async (req, res) => {
+//    const { id_calon } = req.params
+//    try{
+//       const calon = await pool.query(
+//          "select calon.nama, partai.nama_partai FROM partai_calon JOIN calon ON partai_calon.id_calon = calon.id_calon JOIN partai ON partai_calon.id_partai = partai.id_partai WHERE partai_calon.id_calon = $1;", [
+//             id_calon
+//          ]
+//       )
+
+//       res.json(calon.rows)
+//    } catch(err) {
+//       res.json({ message: err })
+//    }
+// }
+
+exports.selectPartaiCalon = async (req, res) => {
+   const { id_calon } = req.params
+   try{
+      const calon = await pool.query(
+         "select partai.nama_partai FROM partai_calon JOIN calon ON partai_calon.id_calon = calon.id_calon JOIN partai ON partai_calon.id_partai = partai.id_partai WHERE partai_calon.id_calon = $1;",[
+            id_calon
+         ])
+      
+         res.json(calon.rows)
+   } catch(err) {
+      res.json({ message: err })
+   }
+}
+
+exports.addRiwayatPendidikanCalon = async (req, res) => {
+   try{
+      const { id_calon } = req.body
+      const { nama_institusi } = req.body
+      const { detail } = req.body
+      const { tahun_mulai } = req.body
+      const { tahun_selesai } = req.body
+
+      const calon = await pool.query(
+         "INSERT INTO riwayat_pendidikan(id_calon, nama_institusi, detail_pendidikan, tahun_mulai_pendidikan , tahun_selesai_pendidikan) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [id_calon, nama_institusi, detail, tahun_mulai, tahun_selesai])
+
+      res.json(calon)
+   } catch (err){
+      console.error(err.message)
+   }
+}
+
+exports.addRiwayatPekerjaanCalon = async (req, res) => {
+   try{
+      const { id_calon } = req.body
+      const { nama_pekerjaan } = req.body
+      const { detail } = req.body
+      const { tahun_mulai } = req.body
+      const { tahun_selesai } = req.body
+
+      const calon = await pool.query(
+         "INSERT INTO riwayat_pekerjaan(id_calon, nama_pekerjaan, detail_pekerjaan, tahun_mulai_pekerjaan, tahun_selesai_pekerjaan) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [id_calon, nama_pekerjaan, detail, tahun_mulai, tahun_selesai])
+
+      res.json(calon)
+   } catch (err){
+      console.error(err.message)
+   }
+}
+
+exports.selectCalonByAdmin = async (req, res) => {
+   try{
+      const calon = await pool.query(
+         'select c.id_calon, c.nama, c.foto, c.slogan, a.id_admin, j.jabatan_tujuan, k.kota, p.provinsi, pend.nama_institusi, pend.detail_pendidikan, pend.tahun_mulai_pendidikan, pend.tahun_selesai_pendidikan, pek.nama_pekerjaan, pek.detail_pekerjaan, pek.tahun_mulai_pekerjaan, pek.tahun_selesai_pekerjaan FROM admins a JOIN calon c on c.id_admin = a.id_admin JOIN jabatan j on c.id_jabatan = j.id_jabatan JOIN kota k on c.id_dapil_kota = k.id_kota JOIN provinsi p on k.id_provinsi = p.id_provinsi JOIN riwayat_pendidikan pend on pend.id_calon = c.id_calon JOIN riwayat_pekerjaan pek on pek.id_calon = c.id_calon  WHERE a.id_admin = $1;', [
+         req.user
+      ])
+
+      res.json(calon.rows[0])
    } catch(err) {
       res.json({ message: err })
    }
--- server/src/routes/calon_route.js
+++ server/src/routes/calon_route.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/calon_controller")
+const authorization = require("../middleware/authorization")
 
 router.get("/", controller.selectAllCalon)
-router.get("/partai", controller.selectPartai)
+router.get("/admin", authorization, controller.selectCalonByAdmin)
 router.post("/", controller.addCalon)
-router.get("/:id", controller.selectSpecificCalon)
+router.get("/:id", controller.selectCalonByUser)
 router.get("/kota/:id_kota", controller.selectCalonByKota)
 router.get("/jabatan/:id_jabatan", controller.selectCalonByJabatan)
+
 router.get("/partai/:id_partai", controller.selectCalonByPartai)
+router.get("/partai/:id_calon", controller.selectPartaiCalon)
+//router.get("/partai", controller.selectPartai)
+
+router.post("/riwayat-pendidikan", controller.addRiwayatPendidikanCalon)
+router.post("/riwayat-pekerjaan", controller.addRiwayatPekerjaanCalon)
 
 module.exports = router
