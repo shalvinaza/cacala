921d162e285799338332236d49c01f2998f4c9e6
aithrajbouty <aithrabouty@gmail.com>
2021-11-12 09:40:15 +0700

    add controller for kota table

--- server/src/controllers/dapil_controller.js
+++ server/src/controllers/dapil_controller.js
@@ -13,48 +13,109 @@ exports.selectAllProvinsi = async (req, res) => {
 }
 
 exports.addProvinsi = async (req, res) => {
-    try{
-       const { provinsi } = req.body
- 
-       const add_provinsi = await pool.query(
-          "INSERT INTO provinsi(provinsi) VALUES($1) RETURNING *",
-          [provinsi]
-       )
- 
-       res.json(add_provinsi)
-    } catch(err) {
-       console.error(err.message)
-    }
+	try{
+		const { provinsi } = req.body
+
+		const add_provinsi = await pool.query(
+			"INSERT INTO provinsi(provinsi) VALUES($1) RETURNING *",
+			[provinsi]
+		)
+
+		res.json(add_provinsi)
+	} catch(err) {
+		console.error(err.message)
+	}
  }
 
 exports.updateProvinsi = async (req, res) => {
-    try{
-        const { id_provinsi } = req.params
-        const { provinsi } = req.body
+	try{
+		const { id_provinsi } = req.params
+		const { provinsi } = req.body
 
-        const update_provinsi = await pool.query(
-            "UPDATE provinsi SET provinsi = $1 WHERE id_provinsi = $2",
-            [provinsi, id_provinsi]
-        )
+		const update_provinsi = await pool.query(
+			"UPDATE provinsi SET provinsi = $1 WHERE id_provinsi = $2",
+			[provinsi, id_provinsi]
+		)
 
-        res.json(update_provinsi)
-    } catch(err) {
-        console.error(err.message)
-    }
+		res.json(update_provinsi)
+	} catch(err) {
+		console.error(err.message)
+	}
 }
 
 exports.deleteProvinsi = async (req, res) => {
-    try{
-        const { id_provinsi } = req.params
+	try{
+		const { id_provinsi } = req.params
 
-        const provinsi = await pool.query(
-            "DELETE FROM provinsi WHERE id_provinsi = $1", [
-                id_provinsi
-            ]
-        )
+		const provinsi = await pool.query(
+			"DELETE FROM provinsi WHERE id_provinsi = $1", [
+					id_provinsi
+			]
+		)
 
-        res.json("Provinsi is deleted")
-    } catch (err) {
-        res.json({ message: err })
-    }
+		res.json("Provinsi is deleted")
+	} catch (err) {
+		res.json({ message: err })
+	}
+}
+
+exports.selectAllKota = async (req, res) => {
+	try{
+		const kota = await pool.query(
+			"SELECT * from kota, provinsi where kota.id_provinsi = provinsi.id_provinsi"
+		)
+
+		res.json(kota.rows)
+	} catch(err){
+		res.json({ message: err })
+	}
+}
+
+ exports.addKota = async (req, res) => {
+	try{
+		const { id_provinsi } = req.body
+		const { kota } = req.body
+
+		const add_kota = await pool.query(
+			"INSERT INTO kota(id_provinsi, kota) VALUES($1, $2) RETURNING *",
+			[id_provinsi, kota]
+		)
+
+		res.json(add_kota)
+	} catch(err) {
+		console.error(err.message)
+	}
+}
+
+exports.updateKota = async (req, res) => {
+	try{
+		const { id_kota } = req.params
+		const { id_provinsi } = req.body
+		const { kota } = req.body
+
+		const update_kota = await pool.query(
+			"UPDATE kota SET id_provinsi = $1, kota = $2 WHERE id_kota = $3",
+			[id_provinsi, kota, id_kota]
+		)
+
+		res.json(update_kota)
+	} catch(err) {
+		console.error(err.message)
+	}
+}
+
+exports.deleteKota = async (req, res) => {
+	try{
+		const { id_kota } = req.params
+
+		const kota = await pool.query(
+			"DELETE FROM kota WHERE id_kota = $1", [
+				id_kota
+			]
+		)
+
+		res.json("Kota is deleted")
+	} catch (err) {
+		res.json({ message: err })
+	}
 }
--- server/src/routes/dapil_route.js
+++ server/src/routes/dapil_route.js
@@ -7,4 +7,9 @@ router.post("/provinsi", controller.addProvinsi)
 router.put("/provinsi/:id_provinsi", controller.updateProvinsi)
 router.delete("/provinsi/:id_provinsi", controller.deleteProvinsi)
 
+router.get("/kota", controller.selectAllKota)
+router.post("/kota/", controller.addKota)
+router.put("/kota/:id_kota", controller.updateKota)
+router.delete("/kota/:id_kota", controller.deleteKota)
+
 module.exports = router
