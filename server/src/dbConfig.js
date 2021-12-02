e11d979203effd163562f2e370fc4ee29ccaf7df
aithrajbouty <aithrabouty@gmail.com>
2021-12-02 15:38:38 +0700

    update dbconfig

--- server/src/dbConfig.js
+++ server/src/dbConfig.js
@@ -3,7 +3,10 @@ require("dotenv").config()
 const { Pool } = require('pg');
 
 const pool = new Pool({
-   connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+   connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+   ssl: {
+      rejectUnauthorized: false
+   }
 }); 
 pool.connect();
 
