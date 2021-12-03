255be56d75590cabdb3378a9ffd3c7f25f0b9896
shalvinaza <shalvinazahwa@gmail.com>
2021-12-03 08:50:37 +0700

    tryna fix reload

--- client/server.js
+++ client/server.js
@@ -4,7 +4,12 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", function (req, res) {
+       res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  }
 
 const port = process.env.PORT || 9090
 app.listen(port)
