b33ac1ed182c59a6023e21e98f4fa643d1756183
shalvinaza <shalvinazahwa@gmail.com>
2021-12-03 09:10:39 +0700

    tryna fix reload

--- client/server.js
+++ client/server.js
@@ -4,9 +4,14 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("/dist"));
+    app.get("/*", function (req, res) {
+       res.sendFile(path.join(__dirname, "/dist/index.html"));
+  });
+  }
 
-const port = process.env.PORT || 9090
+const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('Listening on port: ' + port)
