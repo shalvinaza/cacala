a311d8db7e755bfbdff0d4674ef5c8b1c054450c
shalvinaza <shalvinazahwa@gmail.com>
2021-12-03 08:48:02 +0700

    tryna fix reload

--- client/server.js
+++ client/server.js
@@ -4,8 +4,7 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'/dist'))})
+app.use('/', serveStatic(path.join(__dirname,'/dist')))
 
 const port = process.env.PORT || 9090
 app.listen(port)
