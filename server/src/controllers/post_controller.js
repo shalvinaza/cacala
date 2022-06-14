5398fc404668a39082d6e2ea20da45587474b7d6
shalvinaza <shalvinazahwa@gmail.com>
2022-06-15 01:46:51 +0700

    trying multiple image upload

--- server/src/controllers/post_controller.js
+++ server/src/controllers/post_controller.js
@@ -6,7 +6,7 @@ const app = express()
 
 const storage = multer.diskStorage({
    destination: function(req, file, cb){
-      cb(null, process.cwd() + "/uploads/");
+      cb(null, "server/uploads/");
    },
    filename: function(req, file, cb){
       cb(null, new Date().toISOString().replace(/\s+/g, "-") + file.originalname);
