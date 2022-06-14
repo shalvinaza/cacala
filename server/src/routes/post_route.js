1874e62e543f5aa63a1f6575bd2aae9d248b66a5
shalvinaza <shalvinazahwa@gmail.com>
2022-06-15 02:55:49 +0700

    trying multiple image upload

--- server/src/routes/post_route.js
+++ server/src/routes/post_route.js
@@ -8,7 +8,7 @@ const app = express()
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-       cb(null,'./server/uploads');
+       cb(null,'uploads/');
     },
     filename: function(req, file, cb){
        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
