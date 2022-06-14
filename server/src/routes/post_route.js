114a2d712bbf569142f628791c5098c3fe3f97a0
shalvinaza <shalvinazahwa@gmail.com>
2022-06-15 03:25:35 +0700

    change select all dapi_calon. to dapil.  on server

--- server/src/routes/post_route.js
+++ server/src/routes/post_route.js
@@ -8,10 +8,10 @@ const app = express()
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-       cb(null,'./uploads/');
+       cb(null,path.join(__dirname,'/uploads/'));
     },
     filename: function(req, file, cb){
-       cb(null, new Date() + file.originalname);
+       cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
     }
  });
  
