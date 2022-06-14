0d09f22c2a01ac0e3ee02ad48fde6c9b9dd58010
shalvinaza <shalvinazahwa@gmail.com>
2022-06-15 01:25:13 +0700

    trying multiple image upload

--- server/src/controllers/post_controller.js
+++ server/src/controllers/post_controller.js
@@ -6,10 +6,10 @@ const app = express()
 
 const storage = multer.diskStorage({
    destination: function(req, file, cb){
-      cb(null, 'uploads/');
+      cb(null, './uploads/');
    },
    filename: function(req, file, cb){
-      cb(null, new Date().toISOString() + file.originalname);
+      cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
    }
 });
 
--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ require("dotenv/config")
 app.use(express.json()) //req.body
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/uploads', express.static('uploads'))
 
 //ROUTES//
 const authRoute = require("../src/routes/auth_route")
